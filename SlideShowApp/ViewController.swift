0a9ff1da9756bc021e3779c318b3b78405e9580a
harucandy9 <harucandy0913@gmail.com>
2020-08-02 16:26:31 +0900

    画像の設定、進む戻るボタンの動き設定（１）

--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -10,14 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //画像の準備
+    var imageNo : Int = 0;
+    let images:[String] = ["spring.jpg","summer.jpg","fall.jpg","winter.jpg"]
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    //ロード時
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        ShowImage(self.imageNo)
 
-    @IBOutlet weak var thumbnail: UIImageView!
+    }
     
+    //Action
     @IBAction func unwind(_ segue: UIStoryboardSegue){}
+    
+    @IBAction func buttonNextImage(_ sender: Any) {
+        self.imageNo += 1;
+        ShowImage(self.imageNo)
+    }
+    
+    @IBAction func buttonPreviewImage(_ sender: Any) {
+        self.imageNo -= 1;
+        ShowImage(self.imageNo)
+    }
+    
+    //表示画像の設定
+    func ShowImage (_ no : Int){
+        self.thumbnail.image = UIImage(named: images[no]);
+    }
+    
+
+    
+
+    
+
+    
+
     
 }
 
