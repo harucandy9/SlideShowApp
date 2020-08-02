0d0528b43d0f5187372979f69dbca2bd82ae4169
harucandy9 <harucandy0913@gmail.com>
2020-08-02 15:18:38 +0900

    Initial Commit

---
+++ SlideShowApp/OriginalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OriginalViewController.swift
+//  SlideShowApp
+//
+//  Created by h_nagao on 2020/08/02.
+//  Copyright © 2020 haruka.nagao. All rights reserved.
+//
+
+import UIKit
+
+class OriginalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by h_nagao on 2020/08/02.
+//  Copyright © 2020 haruka.nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue){}
+    
+}
+
