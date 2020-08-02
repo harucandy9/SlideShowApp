41955badc7851205daeb2326f958da0627d8c44b
harucandy9 <harucandy0913@gmail.com>
2020-08-02 18:25:32 +0900

    自動スライドの処理

--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -10,45 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //画像の準備
+    //画像の用意
     var imageNo : Int = 0;
-    let images:[String] = ["spring.jpg","summer.jpg","fall.jpg","winter.jpg"]
+    let images:[String] = ["spring.jpg","summer.jpg","autumn.jpg","winter.jpg"];
+    
+    var timer_sec : Int = 0;
+    var isRunSlide : Bool = false;
     
     @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var buttonPreview: UIButton!
+    @IBOutlet weak var buttonNext: UIButton!
+    @IBOutlet weak var buttonSlide: UIButton!
+    
+    // selector: #selector(updatetimer(_:)) で指定された関数
+    // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+    @objc func updateTimer(_ timer: Timer) {
+        self.timer_sec += 1;
+        if(timer_sec%2 == 0){ self.buttonNextImage(self); }
+    }
     
     //ロード時
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        ShowImage(self.imageNo)
+        ShowImage(0)
 
     }
     
-    //Action
     @IBAction func unwind(_ segue: UIStoryboardSegue){}
     
+    //進むボタンタップ時
     @IBAction func buttonNextImage(_ sender: Any) {
         self.imageNo += 1;
-        ShowImage(self.imageNo)
+        if(self.imageNo >= images.count){ self.imageNo = 0}
+        ShowImage(imageNo)
     }
     
+    //戻るボタンタップ時
     @IBAction func buttonPreviewImage(_ sender: Any) {
         self.imageNo -= 1;
-        ShowImage(self.imageNo)
+        if(self.imageNo < 0){ self.imageNo = self.images.count-1}
+        ShowImage(imageNo)
     }
     
-    //表示画像の設定
-    func ShowImage (_ no : Int){
+    //再生or停止ボタンタップ時
+    @IBAction func buttonStartOrStopSlide(_ sender: Any) {
+        if(isRunSlide == false){StartSlide();}
+        StopSlide();
+    }
+    //自動スライド再生時
+    func StartSlide(){
+        DisableButtons();
+        buttonSlide.setTitle("停止", for: .normal)
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true);
+        isRunSlide = true;
+    }
+    //自動スライド停止時
+    func StopSlide(){
+        EnableButtons();
+        buttonSlide.setTitle("再生", for: .normal)
+        isRunSlide = false;
+        self.timer_sec = 0;
+    }
+    
+    //画像を表示する
+    func ShowImage(_ no : Int){
         self.thumbnail.image = UIImage(named: images[no]);
     }
     
-
+    //戻る進むボタンを有効にする
+    func EnableButtons(){
+        buttonNext.isEnabled = true;
+        buttonPreview.isEnabled = true;
+    }
     
-
-    
-
-    
+    //戻る進むボタンを無効にする
+    func DisableButtons(){
+        buttonNext.isEnabled = false;
+        buttonPreview.isEnabled = false;
+    }
 
     
 }
